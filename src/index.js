e86d6d3dc710f3587329b05a63bf94a1fda5927c
Selva Ganesan <78426427+Selvaganesan123@users.noreply.github.com>
2025-05-07 22:38:15 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/mock-data', (req, res) => {
+  res.json({ message: 'Hello from deployed mock server!' });
+});
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from deployed mock server root!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
